255a3f5be0c90778bd3474d75c8ceef28312718c
Savva <sav.van.ivan3@mail.ru>
2023-11-04 19:07:17 +0300

    first commit

---
+++ src/com/techmeskills/lesson1/FirstClass.java
@@ -0,0 +1,7 @@
+package com.techmeskills.lesson1;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello, team");
+    }
+}
---
+++ src/com/techmeskills/lesson1/SecondClass.java
@@ -0,0 +1,12 @@
+package com.techmeskills.lesson1;
+
+import java.util.Scanner;
+
+public class SecondClass {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String userName = scanner.nextLine();
+
+        System.out.println("Hello, " + userName);
+    }
+}
